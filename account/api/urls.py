fdb555ea193f8d85eae2c10d88c88f3e2ed710ab
Mohit Chaudhary <mohitchauhdary0514@gmail.com>
2025-12-24 23:14:16 +0545

    added:jsonrenderer,implement jwt & access logged user data

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from account.api.views import UserRegistrationView,UserLoginView
+from account.api.views import UserRegistrationView,UserLoginView,UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),    
     path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 
 ]
